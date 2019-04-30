390ca42b1c2f0ec8328a9bdc9f0f9e51de93f53c
ChengqunYang <1521410544@qq.com>
2019-04-30 21:39:08 +0800

    Spring配置代码上传

--- spring/src/spring/demo1/SpringDemo1.java
+++ spring/src/spring/demo1/SpringDemo1.java
@@ -16,8 +16,10 @@ public class SpringDemo1 {
 	 * 传统方法的调用
 	 */
 	public void demo1() {
-		UserDao userDao = new UserDaoImpl();
+		UserDaoImpl userDao = new UserDaoImpl();
+		//此处因为要使用UserDaoImpl的setname方法所以必须生成的是UserDaoImpl的对象
 		//此处是具体实现了UserDaoImpl类,如果要改变底层实现,需要修改代码,所以下面我们采用Spring的方式
+		userDao.setName("liming");  //传统方式需要给变量设置值，要使用setNmae()方法
 		userDao.save();
 	}
 	
@@ -30,6 +32,7 @@ public class SpringDemo1 {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 		UserDao userDao = (UserDao) applicationContext.getBean("UserDao");
 		//此处是到配置文件中去寻找到底实现的是那个,所以不需要修改代码,只需要修改配置文件即可
+		//此处使用di通过配置文件来自动设置对象的属性值
 		userDao.save();
 	}
 }
--- spring/src/spring/demo1/UserDaoImpl.java
+++ spring/src/spring/demo1/UserDaoImpl.java
@@ -4,11 +4,15 @@ package spring.demo1;
  * 用户管理Dao层实现类
  */
 public class UserDaoImpl implements UserDao{
-
+	private String name;
 	
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	@Override
 	public void save() {
-		System.out.println("UserService执行了...");
+		System.out.println("UserService执行了..."+name);
 		
 	}
 
